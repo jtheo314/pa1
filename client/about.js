d8c9e6c64abcf4ec68165f2b183b9dc37260bbb6
First Last <soccerfanatic99@192.168.1.9>
2015-06-18 23:54:52 -0400

    first commit

---
+++ client/about.js
@@ -0,0 +1,11 @@
+var myTeam =
+  [
+    {name:"Jason Theoharis"    , email: "jtheo@brandeis.edu" , position: "Creator/President/CEO" },
+    
+  ];
+
+Template.about.helpers(
+ {
+   team: myTeam
+ }
+)
---
+++ client/display.js
@@ -0,0 +1,38 @@
+var blogs =
+	[
+		{name:"Tim Hickey"    , time: ""	, date: "something" },
+    ];
+
+Template.display.helpers(
+ {
+	record:  function(){
+		return Record.find({}, {sort: {time: -1}, limit: 10});	
+	}
+	blog: blogs
+ }
+)
+
+/*Code for finding the day and time of each post-will be implemented later.
+
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth()+1;
+var year = today.getFullYear();
+
+var hr = today.getHours();
+var min = today.getMinutes();
+if (min <10){
+	min = "0" + min;
+}
+var sec = today.getSeconds();
+if (sec <10){
+	sec = "0" + sec;
+}
+
+Template.schedule.helpers(
+ {
+   activity: function(){return Activities.find({},{sort:{time:1}})},
+   date: month+"/"+day+"/"+year,
+   time: hr+":"+min+":"+sec
+ }
+)
